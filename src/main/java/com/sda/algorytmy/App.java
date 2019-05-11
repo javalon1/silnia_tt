709f1b8532121cc9e9285dccce828eda1de06fcc
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-11 14:37:21 +0100

    Silnia iteracyjnie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -4,10 +4,15 @@ package com.sda.algorytmy;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int silnia = 5;
+        int wynik = 1;
+        for (int i=1; i<=silnia; i++){
+            wynik = wynik * i;
+        }
+        System.out.println(wynik);
     }
 }
