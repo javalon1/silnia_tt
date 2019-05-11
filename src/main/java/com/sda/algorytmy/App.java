05f17fa9aac180696363f1c9013644372394c4fe
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-11 15:01:51 +0100

    silnia rekurencyjnie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -6,13 +6,26 @@ package com.sda.algorytmy;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        int silnia = 5;
+    /*    int silnia = 5;
         int wynik = 1;
         for (int i=1; i<=silnia; i++){
             wynik = wynik * i;
         }
         System.out.println(wynik);
+        */
+        int b=6;
+        System.out.println(silnia(b));
+
     }
+    public static int silnia(int a){
+        if (a<=1){
+            return 1;
+        }
+        else {
+            return a*silnia(a-1);
+        }
+    }
+
 }
