e59ef648e2f48622f3b14970c4670b5e01df4f45
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-11 15:30:21 +0100

    drugi ciąg

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,9 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
     /*    int silnia = 5;
         int wynik = 1;
         for (int i=1; i<=silnia; i++){
@@ -15,29 +12,36 @@ public class App
         }
         System.out.println(wynik);
         */
-        int b=6;
+        int b = 4;
         System.out.println(silnia(b));
         System.out.println(ciag(b));
-
+        System.out.println(ciag2(b));
     }
-    public static int silnia(int a){
-        if (a<=1){
+
+    public static int silnia(int a) {
+        if (a <= 1) {
             return 1;
+        } else {
+            return a * silnia(a - 1);
+        }
+    }
+
+    public static double ciag(int a) {
+        if (a == 1) {
+            return 1;
+        } else if (a == 2) {
+            return 0.5;
+        } else {
+            return -ciag(a - 1) * ciag(a - 2);
+        }
+    }
+
+    public static int ciag2 (int a){
+        if (a==1){
+            return -1;
         }
         else {
-            return a*silnia(a-1);
-        }
-    }
-
-    public static double ciag(int a){
-        if (a==1){
-            return 1;
-        }
-        else if (a==2){
-            return 0.5;
-        }
-        else{
-            return -ciag(a-1)*ciag(a-2);
+            return -ciag2(a-1)*a-3;
         }
     }
 
